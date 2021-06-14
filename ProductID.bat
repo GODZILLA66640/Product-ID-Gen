9ff937d5642ed00507f3934e02730c5a8b3e0b0d
GODZILLA66640 <85907312+GODZILLA66640@users.noreply.github.com>
2021-06-14 19:06:29 -0400

    Update ProductID.bat

--- ProductID.bat
+++ ProductID.bat
@@ -1,14 +1,11 @@
 @echo off
-
  call :isAdmin
-
  if %errorlevel% == 0 (
     goto :run
  ) else (
     echo Requesting administrative privileges...
     goto :UACPrompt
  )
-
  exit /b
 
  :isAdmin
@@ -40,23 +37,35 @@ set _RND=%Random%
 set /A _RND=_RND%%%_Len%
 set _RndAlphaNum=!_RndAlphaNum!!_Alphanumeric:~%_RND%,1!
 if !_count! lss %_RNDLength% goto _loop
+
+:Color Text
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
+call :colorEcho 0a "=========================="
 echo.
-echo ===================================
-echo ProductID is: !_RndAlphaNum!
-echo ===================================
+call :colorEcho 04 "ProductID is !_RndAlphaNum!"
 echo.
-@echo off
+call :colorEcho 0a "=========================="
+echo.
+pause
+exit
+
+:colorEcho
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1i
+
+:Reg DIR
 reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductId /f >Nul
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductId /d !_RndAlphaNum! >Nul
 reg delete "HKLM\SOFTWARE\Microsoft\Internet Explorer\Registration" /v ProductId /f >Nul
 reg add "HKLM\SOFTWARE\Microsoft\Internet Explorer\Registration" /v ProductId /d !_RndAlphaNum! >Nul
-pause
  exit /b
 
  :UACPrompt
    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %~1", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
    "%temp%\getadmin.vbs"
    del "%temp%\getadmin.vbs"
   exit /B`
